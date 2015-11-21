f49a752a6df5eb3abb41dd9ea9abd2594758fec8
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-11-21 23:44:01 +0900

    add validate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
 
   subject { @user }
 
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:email) }
+
+  it { is_expected.to be_valid }
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_addres|
+        @user.email = invalid_addres
+        expect(@user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do 
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email address is already token" do
+    before do 
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { is_expected.not_to be_valid }
+  end
+
 end
 
