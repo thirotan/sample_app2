181eda8a4dff052a6e05ccfd58c265cfc78e44e3
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-11-22 14:26:13 +0900

    finished chapter 6 exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,6 +65,16 @@ RSpec.describe User, type: :model do
     it { is_expected.not_to be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAmPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do 
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
