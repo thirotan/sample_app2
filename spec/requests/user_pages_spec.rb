8206e8eb26be73593790663fc25e9e40659b6138
thirotan <dev@vagrant-centos65.vagrantup.com>
2015-11-28 14:13:37 +0900

    fix user pages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 RSpec.describe "UserPages", type: :request do
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { is_expected.to have_content(user.name) }
+    it { is_expected.to have_title(user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
     
     it { is_expected.to have_content('Sign up') }
     it { is_expected.to have_title(full_title('Sign up')) }
   end
+
 end
